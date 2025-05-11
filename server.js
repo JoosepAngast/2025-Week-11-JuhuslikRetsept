af784b820c71b8b6d3c5d56209e7d00473fa30a0
Joosep Angast <82107834+JoosepAngast@users.noreply.github.com>
2025-05-11 17:02:11 +0300

    Update server.js

--- server.js
+++ server.js
@@ -5,7 +5,6 @@ const fullRecipesRouter = require('./routes/fullRecipes.routes');
 const randomRouter = require('./routes/randomRecipe.routes');
 const app = express();
 
-app.use(express.json());
 
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
